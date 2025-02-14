dc6e1367bb4c9be0b283ca6e4b738c4b109689f1
AmanKaushik77 <100389449+AmanKaushik77@users.noreply.github.com>
2025-02-14 00:39:15 -0800

    Ball Anchor

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,23 +4,30 @@ using UnityEngine.Events;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform launchIndicator;
 
+    private bool isBallLaunched;
     private Rigidbody ballRB;
 
     void Start()
     {
-        // Grabbing a reference to RigidBody
         ballRB = GetComponent<Rigidbody>();
-
-        // Add a listener to the OnSpacePressed event.
-        // When the space key is pressed, the
-        // LaunchBall method will be called.
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
-
+             
+        
     private void LaunchBall()
     {
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        if (isBallLaunched) return;
+        isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
     }
 }
